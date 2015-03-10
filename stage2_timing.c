e1da97a2587d3965ff3c9a42304fb3a0b6708c2a
Junjie Huang <jh954@cd.cs.rutgers.edu>
2015-03-09 23:41:44 -0400

    stage2 finished

--- stage2_timing.c
+++ stage2_timing.c
@@ -4,19 +4,19 @@
 #include "signal.h"
 
 #define N 1000000
-int counts = 10;
+int counts = N;
 
 void handle_signal(int signum)
 {
 	--counts;
-	//__asm__ ("popl %eax\n\t");
-	//__asm__ ("popl %ecx\n\t");
-	//__asm__ ("popl %edx\n\t");
-	//__asm__ ("pushl %ecx\n\t");
-	//__asm__ ("pushl %eax\n\t");
+	__asm__ ("pop %eax\n\t");
+	__asm__ ("pop %ecx\n\t");
+	__asm__ ("pop %edx\n\t");
+	__asm__ ("push %ecx\n\t");
+	__asm__ ("push %eax\n\t");
 	//printf(1,"in handle\n");
 	if(!counts){
-		__asm__ ("movl $0x69, 4(%ebp)\n\t");
+		__asm__ ("movl $0x6e, 8(%ebp)\n\t");
 	}
 	
 }
@@ -33,11 +33,11 @@ int main(int argc, char *argv[])
 	int end = uptime();
 	
 	int elapsedTime = 10000*(end - start);// time in micro-second
-	//int avTime = elapsedTime / counts;
+	int avTime = elapsedTime / N;
 	
-	printf(1, "Traps Performed: %d\n", counts);
+	printf(1, "Traps Performed: %d\n", N);
 	printf(1, "Total Elapsed Time: %d\n", elapsedTime);
-	//printf(1, "Average Time Per Trap: %d\n", avTime);
+	printf(1, "Average Time Per Trap: %d\n", avTime);
 	
 	exit();
 }
